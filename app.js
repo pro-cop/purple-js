708d9def7498e16af37deadc185664a691feede1
Прокопенко Артем Home <pro-cop@ftm.su>
2025-10-01 10:43:07 +0300

    Сделать функцию для настольных игр. Она которая принимает тип dice, который надо бросить: d4, d6, d8, d10, d12, d16, d20 и возвращает случайное целое число на этом интервале с включёнными границами:
    d6 - возможные значения - 1, 2, 3, 4, 5, 6

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 'use strict'
 console.log('Hello world!!!')
+
+
+function throwDice(diceType) {
+    let grainNumber = 0
+    const diceTypeAllowed = new Set([ "D4", "D6", "D8", "D10", "D12", "D16", "D20"])
+    if(!diceTypeAllowed.has(diceType)) {
+        console.error('Invalid dice type')
+        return null
+    }
+
+    grainNumber = parseInt(diceType.slice(1))
+
+    return Math.ceil(Math.random() * grainNumber)
+}
+
+console.log("throwDice", throwDice('D20'))
