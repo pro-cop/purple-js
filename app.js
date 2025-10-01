f09ffc9b59fc426d063b7ce6a1c69cb1ef3b7d19
Прокопенко Артем Home <pro-cop@ftm.su>
2025-10-01 14:30:02 +0300

    fix: исправлена возможность передавать тип кубика в нижнем регистре

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ console.log('Hello world!!!')
 
 
 function throwDice(diceType) {
-    const diceTypeAllowed = new Set([ "D4", "D6", "D8", "D10", "D12", "D16", "D20"])
-    if(!diceTypeAllowed.has(diceType.toUpperCase())) {
+    const diceTypeAllowed = new Set(["D4", "D6", "D8", "D10", "D12", "D16", "D20"])
+    if (!diceTypeAllowed.has(diceType.toUpperCase())) {
         console.error('Invalid dice type')
         return null
     }
 
     const grainNumber = parseInt(diceType.slice(1))
 
-    return Math.ceil(Math.random() * grainNumber)
+    return Math.floor( Math.random() * grainNumber)  + 1
 }
 
 console.log("throwDice", throwDice('D20'))
